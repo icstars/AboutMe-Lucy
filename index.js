af1e137035ffc83e28fe9c880b6bee06ceca6805
lxiongicstars <lxiong@icstars.org>
2023-06-21 19:43:51 -0500

    John assignment

---
+++ index.js
@@ -0,0 +1,43 @@
+// // Variables
+// // let, var, const
+
+// let name = "Lucy";
+
+// name = "Lucy";
+
+// // console.log(name); // Print to the console
+
+// console.log("Hello world");
+
+// const name1 = "pizza";
+// const topping = "pineapple";
+
+// console.log(name1 + " " + "pineapple");
+
+// console.log(name1, topping);
+// console.log(`${name1} ${topping}`);
+// console.log("string" + 1);
+
+// console.log(`${name1} ${topping}`);
+// console.log("32" + "100" + "str");
+
+// //integer
+
+// //booleans
+// const num = 1;
+// console.log(Boolean(num));
+// // false, undefined, null, 0
+
+// let prompt1 = prompt("What is your name?");
+// console.log(prompt1);
+
+// let whereDidSheGo = prompt("Where did she go?");
+// console.log(`${prompt1} went to the ${whereDidSheGo}`);
+
+// alert(`${prompt1} went to the ${whereDidSheGo}`);
+
+let prompt2 = prompt("What is their name?");
+let age = prompt("How old are you?");
+let hobby = prompt("What is your hobby?");
+
+alert(`My name is ${prompt2}, I am ${age}. I like playing ${hobby}.`);
